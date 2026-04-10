a507fd8ce9345713aa514448344c04e6392cbf3c
Ruitian Yan <yanruitian@macbuch-pro.wireless-1x.unc.edu>
2026-04-10 13:51:47 -0400

    export functions

--- R/mktshares.R
+++ R/mktshares.R
@@ -1,3 +1,28 @@
+#' Compute market share in an airport or city
+#'
+#' Compute market share in an airport or city, with flexibility to specify how the 
+#' carrier is defined
+#' 
+#' @param dataframe Dataset to be based market share off of, should have a Passengers column
+#' @param carrier Column name containing carrier name of each ticket, unquoted
+#' @param location Column name containing the origination, unquoted
+#' 
+#' @returns a dataframe with the carrier and orig columns and a market_share 
+#' column that contains the market share of that carrier in that location,
+#' as a proportion.
+#' 
+#' @examplesIf require("tibble")
+#' require("tibble")
+#' data = tibble::tribble(
+#'      ~origin, ~airline, ~Passengers,
+#'      "CLT", "American," 10,
+#'      "RDU","American", 5,
+#'      "RDU", "United", 7
+#' )
+#'
+#' market_shares(data, airline, carrier)
+#' 
+
 #' @export
 mktshares <- function(dataframe, carrier, location){
   mkt_shares = dataframe |>
